db39fb061e6bcda7d2b09feb77fe4800799cabfb
ronpe <ron.peretz1@gmail.com>
2021-08-15 22:07:38 +0300

    implementation of cache on server

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,14 +1,8 @@
-const configuration = {
-    apiKey: 'a454d681',
-    serverPort: 3001
-};
-
 const popularMoviesID = [{imdbID: 'tt0120591',}, {imdbID: 'tt1201607',}, {imdbID: 'tt0099253',},
     {imdbID: 'tt0111161',}, {imdbID: 'tt0167261',}, {imdbID: 'tt0120338',}, {imdbID: 'tt0118158'},
     {imdbID: 'tt0108778',}, {imdbID: 'tt0117571',}, {imdbID: 'tt0114709',}];
 
 
 module.exports = {
-    configuration,
     popularMoviesID
 };
