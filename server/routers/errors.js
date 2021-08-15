ae5519152420bc2fecdc09bba978a6004b4bd961
ronpe <ron.peretz1@gmail.com>
2021-08-15 18:32:42 +0300

    handle with 404 - page not found

--- server/routers/errors.js
+++ server/routers/errors.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', (req, res) => {
-    console.log("Error")
     res.status(404).send("<h1>Oops, Something Went Wrong (404)</h1>");
 });
 
